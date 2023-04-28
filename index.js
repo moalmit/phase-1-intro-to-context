cf888d0dac387f950b895f3d259d5b1c2393fcee
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-04-27 19:50:56 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
+
 // Your code here
+function createEmployeeRecord(arr){
+    const obj = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+    }
+    return obj
+};
+ 
+function createEmployeeRecords(arrOfArrs){  
+    let employeeRecords = arrOfArrs.map( el => createEmployeeRecord(el));
+return employeeRecords 
+};
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+     ];
+
+function createTimeInEvent (employeeRecord, date){ 
+  let obj= {};
+    obj.type = "TimeIn";
+    obj.hour = +(date.slice(11));
+    obj.date = date.substring(0,10);
+  employeeRecord.timeInEvents.push(obj);
+  return employeeRecord
+};
+
+function createTimeOutEvent (employeeRecord, date){ 
+  let obj= {};
+    obj.type = "TimeOut";
+    obj.hour = +(date.slice(11));
+    obj.date = date.substring(0,10);
+  employeeRecord.timeOutEvents.push(obj);
+  return employeeRecord
+};
+
+ function hoursWorkedOnDate (employeeRecord, date){
+  const timeIn = employeeRecord.timeInEvents.find(e=>e.date === date);
+  const timeOut = employeeRecord.timeOutEvents.find(e=>e.date === date);
+  let hours = timeOut.hour - timeIn.hour;
+  return hours/100
+  };
+
+function wagesEarnedOnDate (employeeRecord, date) {
+    return (hoursWorkedOnDate(employeeRecord, date) * (employeeRecord.payPerHour))
+  };
+
+function allWagesFor(employeeRecord){
+  const allWages = employeeRecord.timeInEvents.map(e=>wagesEarnedOnDate(employeeRecord, e.date));
+  return allWages.reduce((accumulator, wage)=> accumulator + wage);
+};
+
+function calculatePayroll (arrOfEmployeeRecords){
+  const total = arrOfEmployeeRecords.map(e=>allWagesFor(e));
+  const sumAll = total.reduce((accumulator, employeeTotal) => accumulator + employeeTotal);
+return sumAll
+};
+
+function calculatePayroll(arrayOfEmployeeRecords){
+  const totalForEachEmployee = arrayOfEmployeeRecords.map(e=>allWagesFor(e));
+return totalForEachEmployee.reduce((accumulator, e)=> accumulator + e)
+}
